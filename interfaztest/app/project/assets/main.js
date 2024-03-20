82be805a54462a376de10e98535f5bdcc3fbc294
Matias Dominguez <mati.d1177@gmail.com>
2024-03-20 17:01:28 -0300

    Cuadrados categorias

---
+++ interfaztest/app/project/assets/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('category1').addEventListener('click', () => {
+      window.location.href = 'app/project/components/pages/page1/page1.html';
+    });
+    document.getElementById('category2').addEventListener('click', () => {
+      window.location.href = '/Interfaz/interfaztest/app/project/components/pages/page1.html';
+    });
+  });
